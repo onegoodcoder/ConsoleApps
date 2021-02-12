1025ccbf87b01980ee93469a548721f1597845e1
admin <summertimematthew@outlook.com>
2021-02-11 20:55:47 -0500

    strings

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string afriend = "matthew";
+            string secondFriend = "brandon";
+            Console.WriteLine($"Hello {afriend} and{secondFriend}");
+
+            Console.WriteLine($"The name {afriend} has {afriend.Length } letters");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length } letters");
+        }
+    }
+}
---
+++ StringTrim/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTrim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "              hello world              ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+             trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+             trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+        }
+    }
+}
