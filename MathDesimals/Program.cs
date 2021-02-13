24d4fcf6bb7332aed1f85c42b9396f80d7778383
admin <summertimematthew@outlook.com>
2021-02-12 21:33:20 -0500

    Math

---
+++ MathDesimals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDesimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+            Console.WriteLine(Math.PI);
+        }
+    }
+}
---
+++ SearchStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string songLyrics = "You say goodbye, and I say hello";
+            bool result = songLyrics.StartsWith("You");
+            Console.WriteLine(result);
+            
+            bool secondResult = songLyrics.EndsWith("hello");
+            Console.WriteLine(secondResult);
+
+
+
+            bool thirdResult = songLyrics.Contains("and");
+            Console.WriteLine(thirdResult);
+
+        }
+    }
+}
