64969b8b013949701e75cad7da5653a8d9923c40
admin <summertimematthew@outlook.com>
2021-02-18 20:52:02 -0500

    BankAccount

--- FibonacciNumbers/Program.cs
+++ FibonacciNumbers/Program.cs
@@ -23,9 +23,11 @@ namespace FibonacciNumbers
 
                 fibonacciNumbers.Add(previous + previous2);
 
-                foreach (var item in fibonacciNumbers)
-                    Console.WriteLine(item);
+               
             }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
 
             //See if you can put together some of the concepts from this and earlier lessons. Expand on what you've built so far with Fibonacci Numbers. Try and write the code to generate the first 20 numbers in the sequence. (As a hint, the 20th Fibonacci number is 6765.)
 
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Matthew",1500);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with balance of {account.Balance}");
+
+
+            account.MakeWithDrawl(800, DateTime.Now, "buying gaming pc");
+            Console.WriteLine($"account balance{account.Balance}");
+        }
+    }
+}
